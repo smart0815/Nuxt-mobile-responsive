d12af660ddc94265c5f7b66c7db4a5495762738b
smart0815 <mileceo@outlook.com>
2021-10-04 21:57:58 +0800

    Update: index and default

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="leading-normal tracking-normal text-white gradient">
+    <NavBar />
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.gradient {
+  /* background: linear-gradient(90deg, #6200ee 0%, #03dac5 100%);  */
+  /* background: linear-gradient(141deg, #EBF4FF 0%, #E6FFFA 100%); */
+  background: linear-gradient(141deg, #EBF4FF 0%, #E6FFFA 100%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <Hero />
+    <Section />
+  </div>
+</template>
